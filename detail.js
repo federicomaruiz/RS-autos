2f51448d622905925059baf97466015a88683fb9
FedeRuiz <federicomaruiz@gmail.com>
2022-04-07 17:32:41 +0200

    Peticion Detail

--- detail.js
+++ detail.js
@@ -1,41 +1,61 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
+const lista = document.getElementById("lista");
 
 async function getCar() {
   try {
-    const response = await fetch(`http://localhost:1337/api/autos${id}`);
+    const url = `http://localhost:1337/api/autos/${id}`;
+
+    const response = await fetch(url);
+
+    const auto = await response.json();
+
     if (!response.ok) {
-      const message = `Error: ${response.status}`;
-      throw new Error(message);
+      throw new Error(`Error: ${response.status}`);
     }
-    const autos = await response.json();
-    printData(autos);
+
+    printData(auto);
   } catch (error) {
     console.log(error);
   }
 }
 
-function printData(autos) {
-  const lista = document.getElementById("lista");
-  lista.innerHTML = "";
-
+function printData(auto) {
   const pMarker = document.createElement("p");
   const pModel = document.createElement("p");
   const pYear = document.createElement("p");
+  const pColour = document.createElement("p");
+  const pCombustible = document.createElement("p");
+  const pPuertas = document.createElement("p");
+  const pTransmision = document.createElement("p");
+  const pMotor = document.createElement("p");
+  const pCarroceria = document.createElement("p");
   const pKm = document.createElement("p");
   const pPrecio = document.createElement("p");
   const img = document.createElement("img");
 
-  pMarker.textContent = auto.attributes.marca;
-  pModel.textContent = auto.attributes.modelo;
-  pYear.textContent = auto.attributes.ano;
-  pKm.textContent = auto.attributes.kilometros;
-  pPrecio.textContent = auto.attributes.precio;
-  img.src = auto.attributes.imagen;
+  pMarker.textContent = auto.data.attributes.marca;
+  pModel.textContent = auto.data.attributes.modelo;
+  pYear.textContent = auto.data.attributes.ano;
+  pColour.textContent =auto.data.attributes.color;
+  pCombustible.textContent =auto.data.attributes.combustible;
+  pPuertas.textContent =auto.data.attributes.puertas;
+  pTransmision.textContent =auto.data.attributes.transmision;
+  pMotor.textContent =auto.data.attributes.motor;
+  pCarroceria.textContent =auto.data.attributes.carroceria;
+  pKm.textContent = auto.data.attributes.kilometros;
+  pPrecio.textContent = auto.data.attributes.precio;
+  img.src = auto.data.attributes.imagen;
 
   lista.appendChild(pMarker);
   lista.appendChild(pModel);
   lista.appendChild(pYear);
+  lista.appendChild(pColour);
+  lista.appendChild(pCombustible);
+  lista.appendChild(pPuertas);
+  lista.appendChild(pTransmision);
+  lista.appendChild(pMotor);
+  lista.appendChild(pCarroceria);
   lista.appendChild(pKm);
   lista.appendChild(pPrecio);
   lista.appendChild(img);
@@ -45,3 +65,5 @@ function printData(autos) {
 
   lista.appendChild(a);
 }
+
+getCar();
