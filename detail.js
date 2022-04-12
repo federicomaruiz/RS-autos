b7820054e85979610fadefb55f0d7c1f4d109a7e
FedeRuiz <federicomaruiz@gmail.com>
2022-04-12 14:37:00 +0200

    Botones

--- detail.js
+++ detail.js
@@ -21,10 +21,13 @@ async function getCar() {
 }
 
 function printData(auto) {
-  const pMarker = document.createElement("p");
-  const pModel = document.createElement("p");
-  const pYear = document.createElement("p");
-  const pColour = document.createElement("p");
+  
+  lista.innerHTML = "";
+
+  const pMarca = document.createElement("p");
+  const pModelo = document.createElement("p");
+  const pAno = document.createElement("p");
+  const pColor = document.createElement("p");
   const pCombustible = document.createElement("p");
   const pPuertas = document.createElement("p");
   const pTransmision = document.createElement("p");
@@ -33,11 +36,12 @@ function printData(auto) {
   const pKm = document.createElement("p");
   const pPrecio = document.createElement("p");
   const img = document.createElement("img");
+ 
 
-  pMarker.textContent = auto.data.attributes.marca;
-  pModel.textContent = auto.data.attributes.modelo;
-  pYear.textContent = auto.data.attributes.ano;
-  pColour.textContent =auto.data.attributes.color;
+  pMarca.textContent = auto.data.attributes.marca;
+  pModelo.textContent = auto.data.attributes.modelo;
+  pAno.textContent = auto.data.attributes.ano;
+  pColor.textContent =auto.data.attributes.color;
   pCombustible.textContent =auto.data.attributes.combustible;
   pPuertas.textContent =auto.data.attributes.puertas;
   pTransmision.textContent =auto.data.attributes.transmision;
@@ -46,11 +50,12 @@ function printData(auto) {
   pKm.textContent = auto.data.attributes.kilometros;
   pPrecio.textContent = auto.data.attributes.precio;
   img.src = auto.data.attributes.imagen;
+  
 
-  lista.appendChild(pMarker);
-  lista.appendChild(pModel);
-  lista.appendChild(pYear);
-  lista.appendChild(pColour);
+  lista.appendChild(pMarca);
+  lista.appendChild(pModelo);
+  lista.appendChild(pAno);
+  lista.appendChild(pColor);
   lista.appendChild(pCombustible);
   lista.appendChild(pPuertas);
   lista.appendChild(pTransmision);
@@ -59,11 +64,18 @@ function printData(auto) {
   lista.appendChild(pKm);
   lista.appendChild(pPrecio);
   lista.appendChild(img);
+  
+  for (const x of auto.data.attributes.imagenes) { 
 
-  a.appendChild(pMarker);
-  a.appendChild(pModel);
+    const imagen = document.createElement("img")
 
-  lista.appendChild(a);
+    imagen.src = x;
+
+  
+    lista.appendChild(imagen);
+
+  }
+
 }
 
 getCar();
--- list.js
+++ list.js
@@ -1,63 +1,197 @@
 const form = document.getElementById("form");
-const year = document.getElementById("year");
-const marker = document.getElementById("marker");
-const model = document.getElementById("model");
-const version = document.getElementById("version");
-const km = document.getElementById("km");
-const btClear = document.getElementById("limpiar");
+const busqueda = document.getElementById("search");
 
-async function search() {
+
+const selectAno = document.getElementById("ano");
+const selectModelo = document.getElementById("modelo");
+const selectVersion = document.getElementById("version");
+const selectKm = document.getElementById("km");
+const selectMarca = document.getElementById("marca");
+
+const btClear = document.getElementById("limpiar");
+const btMostrarFiltros = document.getElementById("mostrarFiltros");
+const btOcultarFiltros = document.getElementById("ocultarFiltros");
+const btBuscar = document.getElementById("btBuscar")
+
+var autosFiltrados;
+let autos;
+let marca;
+let ano;
+
+
+/* filtro de marca por peticion 
+async function searchList() {
   try {
-    let response = await fetch(`http://localhost:1337/api/autos`);
-    const autos = await response.json();
+    const search = document.getElementById("search");
+
+    const response = await fetch(
+      `http://localhost:1337/api/autos?filters[marca][$containsi]=${search.value}`
+    );
+    const busquedaAuto = await response.json();
 
     if (!response.ok) {
-      const message = `Error: ${response.status}`;
-      throw new Error(message);
+      throw new Error(`Error: ${response.status}`);
     }
-
-    printData(autos);
-    console.log(autos);
+    console.log(search.value)
+    printData(busquedaAuto);
   } catch (error) {
     console.log(error);
   }
 }
 
-function printData(autos) {
 
+btBuscar.addEventListener("click", () => {
+  searchList();
+  
+});
+
+*/
+
+btOcultarFiltros.addEventListener("click", () => {
+  selectModelo.style.display = "none";
+  selectMarca.style.display = "none";
+  selectAno.style.display = "none";
+  selectVersion.style.display = "none";
+  selectKm.style.display = "none";
+});
+
+btMostrarFiltros.addEventListener("click", () => {
+  selectModelo.style.display = "inline-block";
+  selectMarca.style.display = "inline-block";
+  selectAno.style.display = "inline-block";
+  selectVersion.style.display = "inline-block";
+  selectKm.style.display = "inline-block";
+
+  limpiar();
+});
+
+btClear.addEventListener("click", limpiar);
+
+function limpiar() {
+  selectAno.value = "";
+  selectMarca.value = "";
+  selectModelo.value = "";
+  selectVersion.value = "";
+  selectKm.value = "";
+  busqueda.value = "";
+
+  search()
+  
+}
+
+selectMarca.addEventListener("change", () => {
+  
+  marca = selectMarca.value;
+
+  // numbers.filter(number => number > 10 )
+  // mirar mayusculas minusculas de value == para que coincidan marca modelo etc
+  // usar if else para concatenar mas de un filtro al mismo tiempo
+
+  autosFiltrados = autos.data.filter( auto => auto.attributes.marca == marca); 
+
+  console.log(autosFiltrados);
+
+  mostrarModelo();
+
+
+});
+
+selectAno.addEventListener("change", () => {
+  ano = selectAno.value;
+
+  // seguir aca mirar como mostrar despues del filtro //
+  autosFiltrados = autos.data.filter( auto => auto.attributes.ano == ano); 
+
+  lista.value = autosFiltrados;
+
+  mostrarModelo();
+
+
+});
+
+function mostrarModelo() {
+  if (marca && ano) {
+    selectModelo.disabled = false;
+  }
+}
+
+async function search() {
+  try {
+    let response = await fetch(`http://localhost:1337/api/autos`);
+    autos = await response.json();
+
+    if (!response.ok) {
+      const message = `Error: ${response.status}`;
+      throw new Error(message);
+    }
+      console.log(autosFiltrados)
+   /*
+          IDEA
+   
+    if (autosFiltrados != null ){
+
+      printData(autosFiltrados)
+    }
+   */
+
+    printData(autos);
+    console.log(autos);
+    
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  
+}
+
+
+function printData(autos) {
+  rellenarModelo();
   const lista = document.getElementById("lista");
   lista.innerHTML = "";
 
   for (const auto of autos.data) {
 
     const a = document.createElement("a");
-    const pMarker = document.createElement("p");
-    const pModel = document.createElement("p");
-    const pYear = document.createElement("p");
+    const pMarca = document.createElement("p");
+    const pModelo = document.createElement("p");
+    const pAno = document.createElement("p");
     const pKm = document.createElement("p");
     const pPrecio = document.createElement("p");
     const img = document.createElement("img");
 
-    pMarker.textContent = auto.attributes.marca;
-    pModel.textContent = auto.attributes.modelo;
-    pYear.textContent = auto.attributes.ano;
+    pMarca.textContent = auto.attributes.marca;
+    pModelo.textContent = auto.attributes.modelo;
+    pAno.textContent = auto.attributes.ano;
     pKm.textContent = auto.attributes.kilometros;
-    pPrecio.textContent = auto.attributes.precio; 
+    pPrecio.textContent = auto.attributes.precio;
     img.src = auto.attributes.imagen;
 
     a.href = `detail.html?id=${auto.id}`;
 
-    lista.appendChild(pMarker);
-    lista.appendChild(pModel);
-    lista.appendChild(pYear);
+    lista.appendChild(pMarca);
+    lista.appendChild(pModelo);
+    lista.appendChild(pAno);
     lista.appendChild(pKm);
     lista.appendChild(pPrecio);
     lista.appendChild(img);
 
-    a.appendChild(pMarker);
-    a.appendChild(pModel);
+    a.appendChild(pMarca);
+    a.appendChild(pModelo);
 
     lista.appendChild(a);
   }
 }
 search();
+
+function rellenarModelo() {
+  for (const x of autos.data) {
+    var opt = document.createElement("option");
+    opt.value = x.attributes.modelo;
+    opt.innerHTML = x.attributes.modelo;
+    selectModelo.appendChild(opt);
+  }
+}
+
+
